c7f1d2e1123d5c0c3b813e5386645d51bc4820c2
proyirvin2333 <proyirvin2333@gmail.com>
2023-07-17 12:18:02 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBQ1KO9ozqoQvnYwvDRIthw8wTwzd5UVN4",
+  authDomain: "bwebu203.firebaseapp.com",
+  projectId: "bwebu203",
+  storageBucket: "bwebu203.appspot.com",
+  messagingSenderId: "46342346563",
+  appId: "1:46342346563:web:651cdb7de0edb648ce81f6",
+  measurementId: "G-C60HFHMRF3"
+});
